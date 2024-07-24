82636a3625f59b9b561e2c3ea4704b0d2828bda4
qwesikhalifa <qwesikhalifa0707@gmail.com>
2024-07-24 04:23:12 -1200

    About Update
    About Page Update

--- src/Pages/About/index.tsx
+++ src/Pages/About/index.tsx
@@ -1,7 +1,25 @@
 const About = () => {
   return (
-    <div>
-      <h1>About Page Cominig Up soon!!</h1>
+    <div className='flex w-full h-auto flex-col items-center justify-center'>
+      <h1>The handy works industry in Accra, Ghana and other regions of the country faces several
+challenges that hinder the efficient connection between freelancers and customers. These
+challenges include limited visibility for freelancers, difficulties in finding qualified
+professionals for customers, lack of trust and reliability, pricing transparency issues, and
+limited feedback mechanisms. These challenges are further exacerbated by the absence of a
+centralized platform that addresses the specific needs of the local market.</h1>
+
+  <p>While these challenges are particularly prevalent in Accra, similar issues persist in other
+regions of Ghana. As the proposed platform extends its reach beyond Accra to other
+regions and eventually the entire country, it is essential to address these problems
+effectively to ensure the success and scalability of the platform.</p>
+
+
+<p> Therefore, there is a pressing need to develop an online website for freelancers in handy
+works centered in Accra, Ghana, that can later expand to other regions and eventually cover the entire country. This platform should provide a centralized hub that addresses the
+challenges faced by freelancers and customers, including limited visibility, inefficient
+customer search, trust and reliability concerns, pricing transparency issues, and limited
+feedback mechanisms. By doing so, the platform will revolutionize the way freelancers and
+customers in the handy works industry connect, collaborate, and thrive in Accra, Ghana, and beyond.</p>
     </div>
   );
 }
