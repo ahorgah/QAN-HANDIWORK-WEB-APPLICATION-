e9d9b4a22b4b091f23c8fa935c5a5c077ece0219
qwesikhalifa <qwesikhalifa0707@gmail.com>
2024-07-30 11:07:26 -1200

    Members Update

---
+++ src/teamData.ts
@@ -0,0 +1,36 @@
+// src/teamData.ts
+import bobo from './img/bobo.jpg';
+import kenny from './img/kenny.jpg';
+import nene from './img/nene.jpg';
+
+export interface TeamMember {
+    id: number;
+    name: string;
+    role: string;
+    image: string;
+  }
+  
+  const teamData: TeamMember[] = [
+    {
+      id: 1,
+      name: "ELIHU NENE ADDO",
+      role: "Project Leader",
+      image: nene,
+    },
+    {
+      id: 2,
+      name: "KENNETH AHORGAH",
+      role: "Lead Developer",
+      image: kenny,
+    },
+    {
+        id: 3,
+        name: "QAADIRI SURAJUDEEN",
+        role: "Assisting Developer",
+        image: bobo,
+      },
+   
+  ];
+  
+  export default teamData;
+  
