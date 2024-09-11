4db7eada9f40feea270f5d6c3f12d70a81e6d8f1
KENNETH AHORGAH <keny.edem@gmail.com>
2024-09-11 13:51:24 +0000

    update in food item (menu)

--- src/components/FoodItem/index.tsx
+++ src/components/FoodItem/index.tsx
@@ -1,16 +1,46 @@
 import { FoodItem } from "../../../types";
 import { motion } from "framer-motion";
 import Action from "./action";
+import { IoMdStar, IoMdStarHalf, IoMdStarOutline } from "react-icons/io";
 export const SingleFoodItem = ({
   item,
   col,
-  admin
+  admin,
 }: {
   item: FoodItem;
   col?: boolean;
-  admin?:boolean
+  admin?: boolean;
 }) => {
-  const { id, title, price, calories, imageURL, description } = item;
+  // const rating = item?.rating ?? 0;
+  const { id, title, price, calories, imageURL, description, rating } = item;
+
+  const Ratings = ({ rating }: { rating: number }) => {
+    // Calculate the number of full stars and whether to show a half star
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating % 1 !== 0; // Check if there's a fractional part (e.g., .5)
+    const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0); // Remaining stars to make 5
+
+    return (
+      <div className="flex items-center">
+        {/* Render full stars */}
+        {Array(fullStars)
+          .fill(0)
+          .map((_, index) => (
+            <IoMdStar key={index} className="text-yellow-500 text-lg" />
+          ))}
+
+        {/* Render half star if needed */}
+        {hasHalfStar && <IoMdStarHalf className="text-yellow-500 text-lg" />}
+
+        {/* Render empty stars */}
+        {Array(emptyStars)
+          .fill(0)
+          .map((_, index) => (
+            <IoMdStarOutline key={index} className="text-yellow-500 text-lg" />
+          ))}
+      </div>
+    );
+  };
 
   return (
     <motion.div
@@ -29,12 +59,12 @@ export const SingleFoodItem = ({
           alt={description}
           src={imageURL}
         />
-      <Action food={item} admin={admin} />
+        <Action food={item} admin={admin} />
       </div>
       <div className="w-full flex items-end justify-end flex-col">
         <p className="text-textColor font-semi-bold text-lg">{title}</p>
         <p className="mt-1 text-sm text-gray-500">{description} </p>
-      {/* {admin && (<p className="mt-1 text-sm text-gray-500">{calories} calories </p>)} */}
+        <Ratings rating={rating || 0} />
         <div className="flex items-center justify-between gap-8 ">
           <p className="text-base text-headingColor font-semibold">
             <span className="text-sm text-red-600">₵</span> {price}
